e455e02880cb120bc52eaa37a84963d0046e1569
sudarshanca <sudarshanreddyca@gmail.com>
2023-12-30 13:32:30 +0530

    resumee

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
          <p>
           Front-end developer with 2 years of expertise in HTML, CSS, JavaScript, and React.js, creating captivating digital experiences. Passionate about anime, exploring diverse narratives and animation styles. Avid gamer with an analytical mindset, appreciating the intricacies of game design. My unique blend of technical proficiency and creative interests defines my approach to front-end development.
          </p>
-         <a href="https://drive.google.com/file/d/102WByhuenqq1m0fJJAyXafT7fPO_hAAh/view?usp=sharing"  target='_blank' >Resume</a>
+         <a href="https://drive.google.com/file/d/1ga8N79C6o1G6KLq5i2kr014HwnxgxdXq/view?usp=sharing"  target='_blank' >Resume</a>
          <a href={resum} download>Click to download</a>
         </div>
     </>
