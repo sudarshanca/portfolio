be5c270ed23b3ea0b552a3dd65c12fca2db1be63
sudarshanca <sudarshanreddyca@gmail.com>
2024-03-09 12:40:07 +0530

    f

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleNav() {
-  let toggleBtn = document.querySelector('.toggle-button');
+  let toggleBtn = document.querySelector('.hanburger-menu');
   let links = document.querySelector('.links');
   toggleBtn.classList.toggle('cross');
   links.classList.toggle('show');
