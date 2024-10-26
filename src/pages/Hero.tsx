abcdacd87a709507a9c860669f0506c9fad47956
sudarshanca <sudarshanreddyca@gmail.com>
2024-10-26 19:06:00 +0530

    header

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
                 focus on creating (and occasionally designing) exceptional
                 digital experiences that are fast, accessible, visually
                 appealing, and responsive. Even though I have been creating web
-                applications for over 7 years, I still love it as if it was
+                applications for over 1 years, I still love it as if it was
                 something new.
               </p>
             </div>
